0b403faa44f23138fb1015399acd9122d03d8718
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-15 16:04:29 -0300

    FIX: Bugs corrigidos

--- core/perfil/admin.py
+++ core/perfil/admin.py
@@ -1,3 +1,12 @@
+from core.perfil.models import Area, SubArea, Perfil, Pro, MyProjects, ChoiceProject, MyCompetency, Hability, Nacionality
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Area)
+admin.site.register(SubArea)
+admin.site.register(Perfil)
+admin.site.register(Pro)
+#admin.site.register(MyProjects)
+admin.site.register(ChoiceProject)
+admin.site.register(MyCompetency)
+admin.site.register(Hability)
+admin.site.register(Nacionality)
--- core/project/models.py
+++ core/project/models.py
@@ -1,40 +1,51 @@
 from django.db import models
 from core.user.models import User
+
+class IntegrationType(models.Model):
+    name = models.CharField(max_length=255)
 class Project(models.Model):
-    area = models.CharField(max_length=45)
-    sub_area = models.CharField(max_length=45)
-    title = models.CharField(max_length=45)
-    subTitlte = models.CharField(max_length=45)
+    theme = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    context = models.CharField(max_length=45)
-    demands = models.CharField(max_length=45)
-    nivel_development = models.CharField(max_length=45)
-    need_api_integration = models.CharField(max_length=45)
-    need_frameworks = models.CharField(max_length=45)
-    contractor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="contractor")
-
-    class NivelExperience(models.IntegerChoices):
-        INICIANTE = 1, "Iniciante"
-        INTERMEDIARIO = 2, "INTERMEDIARIO"
-        EXPERIENTE = 3, "EXPERIENTE"
-        ESPECIALISTA = 4, "ESPECIALISTA"
-
-    experiente_required = models.IntegerField(("Experience"), choices=NivelExperience.choices, default=NivelExperience.INTERMEDIARIO)
-    
-    class Budget(models.IntegerChoices):
-        MINIMAL = 1, "100 - 1000"
-        MEDIUM = 2, "1000 - 5000"
-        BIG = 3, "5000 - 10000"
-        GIANT = 4, "10001 - 10000000000000000000000"
-    budget = models.IntegerField(("Budget"), choices=Budget.choices, default=Budget.MEDIUM)
-
-    class Size(models.IntegerChoices):
-        SMALL = 1, "Small"
-        MEDIUM = 2, "Medium"
-        BIG = 3, "Big"
-        GIANT = 4, "Giant"
-
-    size = models.IntegerField(("Size"),choices=Size.choices, default=Size.MEDIUM)
+    context = models.CharField(max_length=255)
+    class Function(models.IntegerChoices):
+        WEB_DESIGN = 1, "Web_design"
+        MOBILE_DESIGN = 2, "Mobile_design"
+        MANAGER = 3, "Manager"
+        OTHER = 4, "Other"
+    function = models.IntegerField(("Function"), choices=Function.choices, default=Function.OTHER)
+    class DevelopmentLevel(models.IntegerChoices):
+        INIT = 1, "I don't have nothing"
+        CONCEPT = 2, "I have a idea"
+        READY  = 3, "I have a design"
+    development_level = models.IntegerField("Level", choices=DevelopmentLevel.choices, default=DevelopmentLevel.INIT)
+    class SpecialResource(models.IntegerChoices):
+        YES = 1, "Yes"
+        PERHAPS = 2, "Perhaps"
+        NO = 3, "NO"
+    special_resouce = models.IntegerField(('Special Resource'), choices=SpecialResource.choices, default=SpecialResource.NO)
+    class ExperienceLevel(models.IntegerChoices):
+        BEGINNER = 1, "Iniciante"
+        INTERMEDIATE = 2, "Intermediário"
+        EXPERIENCED = 3, "Experiente"
+        EXPERT = 4, "Especialista"
+    experience_level = models.IntegerField(choices=ExperienceLevel.choices,default=ExperienceLevel.BEGINNER)
+    class ProjectSize(models.IntegerChoices):
+        SMALL = 1, "Pequeno"
+        MEDIUM = 2, "Intermediário"
+        LARGE = 3, "Grande"
+        GIANT = 4, "Gigante"
+    project_size = models.IntegerField(choices=ProjectSize.choices, default=ProjectSize.SMALL)
+    class BudgetRange(models.IntegerChoices):
+        RANGE_100_1000 = 1, "R$100 - 1.000"
+        RANGE_1000_5000 = 2, "R$1.000 - 5.000"
+        RANGE_5000_10000 = 3, "R$5.000 - 10.000"
+        OVER_10000 = 4, "Mais de R$10.000"
+    budget_range = models.IntegerField(choices=BudgetRange.choices,default=BudgetRange.RANGE_100_1000)
+    delivery = models.DateField(auto_now_add=True, null=True, blank=True)
+class ProjectIntegration(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    integration = models.ForeignKey(IntegrationType, on_delete=models.PROTECT)
 
 
     
