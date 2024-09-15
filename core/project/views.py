f4acde0b69875ee10b766f0a8fa888c569f549e9
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-15 18:36:24 -0300

    FEAT: CHORE: Cadastro de projeto finalizado

--- core/project/views.py
+++ core/project/views.py
@@ -7,6 +7,10 @@ from django.core.mail import send_mail, send_mass_mail
 from rest_framework.response import Response
 from rest_framework import status
 from core.perfil.models import Perfil
+from django.core.mail import send_mass_mail
+from rest_framework.response import Response
+from core.perfil.models import Area
+
 class ProjectView(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
@@ -27,8 +31,32 @@ class ProjectView(ModelViewSet):
                 from_email=from_email
             )
 
-    @property
-    def searchUser(projectType):
-        users = Perfil.objects.get()
+    @receiver(post_save, sender=Project)
+    def warnPossibleFreelancer(sender, instance, created, **kwargs):
+        try:
+            id_area = Area.objects.get(name=instance.theme).pk
+            print(id_area)
+            users = Perfil.objects.filter(area=id_area)
+            print(users)
+            subject = 'Projeto do seu interesse'
+            message = f'Ei, há um projeto que possa ser do seu interesse: {instance.title}'
+            from_email = "martinsbarroskaua85@gmail.com"
+            recipient_list = [user.user.email for user in users]
+            print(recipient_list)
+            send_mass_mail(
+                (subject, message, from_email, recipient_list),
+                fail_silently=False,
+            )
+            
+            return Response({"message": "emails enviados com sucesso"}, status=status.HTTP_200_OK)
         
+        except Area.DoesNotExist:
+            return Response({"message": "Área não encontrada"}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            return Response({"message": f"Houve um erro ao enviar o e-mail: {error}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
+
+        
+
+
 
