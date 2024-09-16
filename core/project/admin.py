5508e15e589250747456078b7dd9c78a07769d0c
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-16 11:51:08 -0300

    FEAT: Creat project models and API

--- core/project/admin.py
+++ core/project/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from core.project.models import Project
 # Register your models here.
+
+
+admin.site.register(Project)
--- core/project/views.py
+++ core/project/views.py
@@ -21,7 +21,7 @@ class ProjectView(ModelViewSet):
             message = f"Ei, você cadastrou um novo projeto!"
             subject = "Novo projeto cadastrado"
             recipient_list = [instance.contractor.email]
-            print(recipient_list)
+            (recipient_list)
             from_email = "martinsbarroskaua85@gmail.com"
             
             send_mail(
@@ -30,32 +30,26 @@ class ProjectView(ModelViewSet):
                 recipient_list=recipient_list,
                 from_email=from_email
              )
+            id_area = Area.objects.get(name=instance.theme).pk
+            print(id_area)
+            users = Perfil.objects.filter(area=id_area)
+            print(users)
+            subject = 'Projeto do seu interesse'
+            message = f'Ei, há um projeto que possa ser do seu interesse: {instance.title}'
+            from_email = "martinsbarroskaua85@gmail.com"
+            recipient_list = [user.user.email for user in users]
+            print(recipient_list)
             try:
-                id_area = Area.objects.get(name=instance.theme).pk
-                print(id_area)
-                users = Perfil.objects.filter(area=id_area)
-                print(users)
-                subject = 'Projeto do seu interesse'
-                message = f'Ei, há um projeto que possa ser do seu interesse: {instance.title}'
-                from_email = "martinsbarroskaua85@gmail.com"
-                recipient_list = [user.user.email for user in users]
-                print(recipient_list)
-                send_mass_mail(
-                    (recipient_list,
-                    message,
-                    from_email,
-                    subject,
-                    ),
-                    fail_silently=False,
+                send_mail (
+                    recipient_list=recipient_list,
+                    message=message,
+                    from_email=from_email,
+                    subject=subject
                 )
-                        
             except Area.DoesNotExist:
-                return Response({"message": "Área não encontrada"}, status=status.HTTP_404_NOT_FOUND)
-            except Exception as error:
-                return Response({"message": f"Houve um erro ao enviar o e-mail: {error}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-                
-
-            
-
+                return Response({"message": "Não existe nenhum usuário com essa classificação"}, status=status.HTTP_404_NOT_FOUND)
+            except BaseException as error:
+                return Response({"message": f"{error}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"email enviado!"})
 
 
