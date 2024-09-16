cb42d655746b81a5eb7d79717314214963f5b2f7
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-16 16:57:30 -0300

    Get feito de maneira específica

--- core/proposal/serializer.py
+++ core/proposal/serializer.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from core.proposal.models import Proposal, Language
 
 class ProposalSerializer(ModelSerializer):
