60792626f5c9e092bf613812f427ebbfe84e8147
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-17 15:00:00 -0300

    CHORE: list na api de criar propostas personalizadas ordenandos as propostas para cada contractor

--- core/proposal/views.py
+++ core/proposal/views.py
@@ -5,17 +5,25 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from rest_framework.response import Response
+from core.project.models import Project
+from rest_framework import status
+from rest_framework.decorators import action
 
 class ProposalView(ModelViewSet):
     queryset = Proposal.objects.all()
     serializer_class = ProposalSerializer
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serialzer = self.get_serializer(queryset, many=True)
-        print()
-        return Response(serialzer.data)
+        email_contratante = request.query_params.get('email', None)
+
+        if not email_contratante:
+            return Response({"message:": "O email do contrante é necessário"}, status=status.HTTP_400_BAD_REQUEST)
         
+        queryset = self.get_queryset().filter(
+            project__contractor__email =email_contratante
+        )
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     @receiver(post_save, sender=Proposal)
     def newProposal(sender, instance, created, **kwargs):
@@ -34,3 +42,4 @@ class ProposalView(ModelViewSet):
 class LanguageView(ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
