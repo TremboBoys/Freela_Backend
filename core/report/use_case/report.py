f4392d5389e3246d0f7b719851943cb901c07cdc
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-23 13:43:23 -0300

    REFACTOR: Geração de pdf não está mais sendo salvo no cloudinary

--- core/report/use_case/report.py
+++ core/report/use_case/report.py
@@ -4,27 +4,28 @@ import cloudinary, cloudinary.uploader
 import io
 
 def generate_pdf(title:str, text:str,name_freelancer:str, date_finished:str):
-    pdf_buffer = io.BytesIO()
-    pdf = canvas.Canvas(pdf_buffer)
+    #pdf_buffer = io.BytesIO()
+    #pdf = canvas.Canvas(pdf_buffer)
     x = 100
     y = 750  
     data = [title, text, name_freelancer, date_finished]
+    pdf = canvas.Canvas(f"{data[0]}.pdf")
 
     for row in data:
         pdf.drawString(x, y, row)
         y -= 20  
-
     pdf.save() 
 
-    pdf_buffer.seek(0)
+    #pdf_buffer.seek(0)
 
-    upload_result = cloudinary.uploader.upload(
+    """pload_result = cloudinary.uploader.upload(
         pdf_buffer, 
         resource_type='raw', 
         public_id=f"{data[0].replace(' ', '_')}.pdf"
-    )
+    )"""
     
-    return upload_result['secure_url']
+    #   return upload_result['secure_url']
+    return pdf
 
 
 
--- core/report/views.py
+++ core/report/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from core.report.models import Report
 from core.report.serializer import ReportSerializer
 from rest_framework.decorators import action
+
 class ReportViewSet(ModelViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
