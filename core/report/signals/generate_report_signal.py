2a1bdb3f720305a2b9163f128b19ed7807035db5
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-24 15:16:41 -0300

    REFACTOR: pdf refatorado

--- core/report/signals/generate_report_signal.py
+++ core/report/signals/generate_report_signal.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from django.core.mail import send_mail
 from rest_framework import status
 import requests
+import reportlab
+from django.core.mail import EmailMessage
 
 @receiver(post_save, sender=Report)
 def generateReport(sender, instance, **kwargs):
@@ -16,15 +18,13 @@ def generateReport(sender, instance, **kwargs):
         
     try:
         subject = 'Relatório'
-        message = pdf
+        message = f"Olá, o freelancer {instance.accept_proposal.perfil.user.name} enviou um relatório"
         recipient_list = [instance.accept_proposal.proposal.project.contractor.email]
         from_email = "martinsbarroskaua85@gmail.com"
-        send_mail (
-            subject=subject,
-            message=message,
-            recipient_list=recipient_list,
-            from_email=from_email,
-        )
+
+        email = EmailMessage(subject=subject, body=message, from_email=from_email, reply_to=recipient_list)
+        email.attach_file(pdf)
+        email.send()
     except BaseException as error:
         return Response({"message": f"Houve um erro!!!!: {str(error)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
--- core/report/use_case/report.py
+++ core/report/use_case/report.py
@@ -2,27 +2,18 @@ from reportlab.pdfgen import canvas
 import os
 import cloudinary, cloudinary.uploader
 import io
+from django.conf import settings
+import os
 
 def generate_pdf(title:str, text:str,name_freelancer:str, date_finished:str):
-    #pdf_buffer = io.BytesIO()
-    #pdf = canvas.Canvas(pdf_buffer)
+    data = [title, text, name_freelancer, date_finished]
+
     x = 100
     y = 750  
-    data = [title, text, name_freelancer, date_finished]
     pdf = canvas.Canvas(f"{data[0]}.pdf")
 
     for row in data:
         pdf.drawString(x, y, row)
         y -= 20  
     pdf.save() 
-
-    #pdf_buffer.seek(0)
-
-    """pload_result = cloudinary.uploader.upload(
-        pdf_buffer, 
-        resource_type='raw', 
-        public_id=f"{data[0].replace(' ', '_')}.pdf"
-    )"""
-    
-    #   return upload_result['secure_url']
     return pdf
