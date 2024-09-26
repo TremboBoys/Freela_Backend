c71853aa022d0f2b6e398e29035e4554ec5689a3
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-26 14:15:15 -0300

    CHORE: html is sending for email with pdf

--- core/report/signals/generate_report_signal.py
+++ core/report/signals/generate_report_signal.py
@@ -4,9 +4,7 @@ from core.report.models import Report
 from core.report.use_case.report import generate_pdf
 from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.template.loader import render_to_string
-import logging
-
-logger = logging.getLogger(__name__)
+from django.utils.html import strip_tags
 
 @receiver(post_save, sender=Report)
 def generate_report(sender, instance, **kwargs):
@@ -19,19 +17,25 @@ def generate_report(sender, instance, **kwargs):
             date_finished="10"
         )
         
-        html_message = render_to_string('template/html/accept_email.html', {
-            'name': f"Olá, o freelancer {freelancer_name} enviou um relatório"
+        print('A')
+        html_message = render_to_string('html/accept_email.html', {
+                'name': f'{instance.accept_proposal.proposal.perfil.user.email}'
         })
-        
+        print('B')
+        text_content = strip_tags(html_message)
+        print('c')
         subject = 'Relatório'
         recipient_list = [instance.accept_proposal.proposal.project.contractor.email]
         from_email = "martinsbarroskaua85@gmail.com"
         
-        email = EmailMultiAlternatives(subject=subject, to=recipient_list, from_email=from_email)
+        print('d')
+        email = EmailMultiAlternatives(subject=subject, to=recipient_list, from_email=from_email, body=text_content)
+        print('e')
         email.attach('report_{}.pdf'.format(instance.id), buffer.getvalue(), 'application/pdf')
+        print('f')
         email.attach_alternative(html_message, "text/html")
-
+        print("G")
         email.send()
-        
+        print('H')
     except Exception as error:
-        logger.error(f"Erro ao gerar e enviar relatório: {error}")
+        print(f"Erro ao gerar e enviar relatório: {error}")
