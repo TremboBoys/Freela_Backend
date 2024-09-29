913a93bec626edcbe0e52709ef64de474331372e
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-09-28 22:23:58 -0300

    CHORE: Special update mixin for accept report when this accept

--- utils/mixins/update_report.py
+++ utils/mixins/update_report.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from core.project.models import Project
 
+
 class UpdateReportModelMixin:
     """
     Update a model instance.
@@ -8,20 +9,17 @@ class UpdateReportModelMixin:
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        print(instance)
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        print(serializer)
+        serializer.is_valid(raise_exception=True)
+
         accept = serializer.validated_data.get('accept_proposal')
-        print(accept)
         is_accept = serializer.validated_data.get('is_accept')
-        print(is_accept)
 
         if is_accept == True:
             project = accept.proposal.project
             project.status = 3
             project.save()
             
-        serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
         if getattr(instance, '_prefetched_objects_cache', None):
