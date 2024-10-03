86dbce7f2e4e3121a9f059118a7e087954152e7d
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-10-03 14:51:11 -0300

    CHORE: Login with passage

--- core/user/authentication.py
+++ core/user/authentication.py
@@ -1,34 +1,30 @@
-from django.contrib.auth import authenticate
-from rest_framework import serializers
-from rest_framework.views import APIView
+from passageidentity import Passage
+from config import settings
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import login
+from rest_framework.views import APIView
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+app_id = settings.PASSAGE_APP_ID
+app_key = settings.PASSAGE_API_KEY
 
-def validate(self, data):
-    username = data.get("username")
-    password = data.get("password")
+passage = Passage(api_key=app_key, app_id=app_id)
 
-    if username and password:
-        user = authenticate(username=username, password=password)
-        if user is None:
-            raise serializers.ValidationError("Invalid credentials")
-    else:
-        raise serializers.ValidationError("Fields are required")
+def authenticate_user(token):
+    try:
+        user_id = passage.authenticate_request(token)
+        return user_id
+    except Exception as error: 
+        return None
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        token = request.data.get('token')
+
+        if not token:
+            return Response({"message": "Token not found"}, status=status.HTTP_400_BAD_REQUEST)
         
-    data['user'] = user
-    return data
-
-class LoginView(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data["user"]
-            login(request, user)
-            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        if authenticate_user(token=token):
+            return Response({"message": "Logged"})
+        else:
+            return Response({"message": "Not allowed"}, status=status.HTTP_401_UNAUTHORIZED)
+    
