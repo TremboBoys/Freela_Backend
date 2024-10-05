64e3ef6233ca853568efb5bab9d9122568adcb0a
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-10-05 15:00:45 -0300

    REFACTOR: User

--- core/user/authentication.py
+++ core/user/authentication.py
@@ -3,28 +3,26 @@ from config import settings
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from core.user.models import User
 
-app_id = settings.PASSAGE_APP_ID
-app_key = settings.PASSAGE_API_KEY
 
-passage = Passage(api_key=app_key, app_id=app_id)
-
-def authenticate_user(token):
+def authenticate_user(email):
     try:
-        user_id = passage.authenticate_request(token)
-        return user_id
-    except Exception as error: 
+        user = User.objects.get(email=email)
+    except User.DoesNotExist as error:
         return None
+
+    print("User logged")
 
 class LoginAPIView(APIView):
     def post(self, request):
-        token = request.data.get('token')
+        email = request.data.get('email')
 
-        if not token:
-            return Response({"message": "Token not found"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if authenticate_user(token=token):
-            return Response({"message": "Logged"})
+        if not email:
+            return Response({"message": "Email is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if authenticate_user(email=email):
+            return Response({"message": "Logged"}, status=status.HTTP_200_OK)
         else:
             return Response({"message": "Not allowed"}, status=status.HTTP_401_UNAUTHORIZED)
     
