9e8a37b9a31a23fc0922281bea1e6db59131d1d6
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-10-24 14:51:42 -0300

    FEAT: Create ads models and API

--- core/ads/models.py
+++ core/ads/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from uploader.models.image import Image
 from core.perfil.models import Perfil
 
+class AdsCategory(models.Model):
+    name = models.CharField(max_length=255)
+
 class Ads(models.Model):
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     links = models.URLField(max_length=255, blank=True)
     perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
-
-class AdsCategory(models.Model):
-    name = models.CharField(max_length=255)
+    target_audience = models.CharField(max_length=255)
+    ads_category = models.ForeignKey(AdsCategory, on_delete=models.CASCADE)
+    
--- core/ads/views.py
+++ core/ads/views.py
@@ -1,11 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from utils.viewset.create_ai import AdsModelViewSet
 from core.ads.models import Ads, AdsCategory
 from core.ads.serializer import AdsSerializer, AdsCategorySerializer
-
-class AdsViewSet(ModelViewSet):
+class AdsViewSet(AdsModelViewSet):
     queryset = Ads.objects.all()
     serializer_class = AdsSerializer
 
 class AdsCategoryViewSet(ModelViewSet):
     queryset = AdsCategory.objects.all()
     serializer_class = AdsCategorySerializer
+
