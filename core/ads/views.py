2c57dfb80144cc7b42c2b0ce0e9810923882d045
kaubarros-24 <martinsbarroskaua@gmail.com>
2024-11-04 17:13:51 -0300

    RESTARTED APLICATION

--- core/ads/views.py
+++ core/ads/views.py
@@ -1,8 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
-from utils.viewset.create_ai import AdsModelViewSet
 from core.ads.models import Ads, AdsCategory
 from core.ads.serializer import AdsSerializer, AdsCategorySerializer
-class AdsViewSet(AdsModelViewSet):
+class AdsViewSet(ModelViewSet):
     queryset = Ads.objects.all()
     serializer_class = AdsSerializer
 
