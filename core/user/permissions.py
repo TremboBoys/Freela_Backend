a2b61318c2aa8f9ff8a47bda761335ed3c5627d4
kaubarros-24 <martinsbarroskaua@gmail.com>
2024-11-13 13:57:36 -0300

    FIX: Conflicts

--- core/user/permissions.py
+++ core/user/permissions.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.models import Group
 
-admin_group = Group.objects.get(name='admin')
-freelancer_group = Group.objects.get(name="freelancers")
-contratante = Group.objects.get(name="contractors")
+
+admin_group = Group.objects.get_or_create(name='admin')
+
+freelancer_group = Group.objects.get_or_create(name="freelancers")
+contratante = Group.objects.get_or_create(name="contractors")
 
 
 
