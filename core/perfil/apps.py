435376d34051218c722d5122d1065fcaa52d4cd0
kaubarros-24 <martinsbarroskaua@gmail.com>
2024-11-16 16:05:42 -0300

    CHORE: Change templates for perfi

--- core/perfil/apps.py
+++ core/perfil/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PerfilConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.perfil'
+
+    def ready(self) -> None:
+        import core.perfil.signals.email_update
