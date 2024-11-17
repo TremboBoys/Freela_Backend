bc6a0a973a13f8ff39a57c0ae92ef57759198376
kaubarros-24 <martinsbarroskaua@gmail.com>
2024-11-17 13:37:56 -0300

    CHORE: Change report models

--- utils/mixins/report_mixin.py
+++ utils/mixins/report_mixin.py
@@ -1,6 +1,13 @@
 class ReportMixin:
-    def process_instance(self, instance):
-        title = getattr(instance, 'title')
-        text_body = getattr(instance, 'text_body')
-        
-        return instance
+    def process_instance(self, attr):
+        print("Conteúdo do objeto:", attr)
+        print("Tipo do objeto:", type(attr))
+        try:
+            title = attr.title
+            text_body = self.attr.text_body
+        except AttributeError:
+            print("Erro: O objeto não possui os atributos 'title' ou 'text_body'.")
+            return attr
+        print("Olá, sou uma barata!")
+        print(title, text_body)
+        return attr
