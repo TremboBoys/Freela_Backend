34998fb0d16d0ad0ada6751920e0cf6e0e69b8b3
kaubarros-24 <martinsbarroskaua@gmail.com>
2024-11-17 15:28:40 -0300

    REFACTOR: Refactor fonts and fontsize of pdf buffer

--- core/report/signals/generate_report_signal.py
+++ core/report/signals/generate_report_signal.py
@@ -6,29 +6,19 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from core.report.use_case.ai_translate import ai_translate
-from utils.mixins.report_mixin import ReportMixin
 
-class ReportHandler(ReportMixin):
-    pass
 
-report_handler = ReportHandler()
 
 @receiver(pre_save, sender=Report)
 def generate_report(sender, instance, **kwargs):
-    print("Estou sendo chamado!")
-    instance = report_handler.process_instance(instance)
     language = instance.accept_proposal.proposal.language.name
     regular_text = instance.text_body
     title = instance.title
-    print(instance.title, instance.text_body)
 
     new_text = ai_translate(text=regular_text, target_language=language)
     new_title = ai_translate(text=title, target_language=language)
 
-    instance.text_body = new_text
-    instance.title = new_title
 
-    print(new_text, new_title)
     try:
         freelancer_name = str(instance.accept_proposal.proposal.perfil.user.name)
 
--- utils/mixins/create_report.py
+++ utils/mixins/create_report.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.settings import api_settings
 from core.report.models import Report
 from core.proposal.models import AcceptProposal
 
@@ -8,13 +7,15 @@ class CreateReportModelModelMixin:
     def create(self, request, *args, **kwargs):
         is_accept = request.data.get('is_accept', False)
         accept_proposal = request.data.get('accept_proposal')
+        title = request.data.get('title')
+        text = request.data.get('text_body')
         
         try:
             proposal = AcceptProposal.objects.get(pk=accept_proposal)
         except AcceptProposal.DoesNotExist:
             return Response({"message": "Proposal Doens't exists"}, status=status.HTTP_404_NOT_FOUND)
     
-        if not accept_proposal:
+        if not accept_proposal or not title or not text:
             return Response({"message": "is_accept or accept_proposal is required"}, status=status.HTTP_400_BAD_REQUEST)
         
         existing_report = Report.objects.filter(accept_proposal=proposal).first() 
@@ -22,7 +23,7 @@ class CreateReportModelModelMixin:
             return Response({"message": "Report for this project is accept"}, status=status.HTTP_412_PRECONDITION_FAILED)
 
         try:
-           report = Report.objects.create(is_accept=is_accept, accept_proposal=proposal)
+           report = Report.objects.create(is_accept=is_accept, accept_proposal=proposal, title=title, text_body=text)
         except Exception as error:
             return Response({"message": f"Has a error in create report: {error}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
