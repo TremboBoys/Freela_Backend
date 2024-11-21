67d17799a9f8826c34476fd5c66e29c007b42d3d
kaubarros-24 <martinsbarroskaua@gmail.com>
2024-11-21 18:16:09 -0300

    CHORE: Implementing update method in put in address api

--- core/pay/use_case/pix.py
+++ core/pay/use_case/pix.py
@@ -38,7 +38,7 @@ def get_address(email):
     except ValueError:
         return {"error": "Invalid JSON received from external service"}
 
-def update_address(id_address, street_name, street_number, complement_address, cellphone_number, neighborhood_name, city_name, name_payer, email_payer, zip_code, old_email):
+def update_address(id_address, street_name, street_number, complement_address, cellphone_number, neighborhood_name, city_name, name_payer, email_payer, zip_code):
     data = {
         "street_name": street_name,
         "street_number": street_number,
@@ -51,28 +51,6 @@ def update_address(id_address, street_name, street_number, complement_address, c
         "zip_code": zip_code 
     }
     
-    user = User.objects.filter(email=old_email).first()
-    
-    if not user:
-        return 404
-    
-    if user.email != email_payer:
-        user.email = email_payer
-        user.save()
-    
-    user = User.objects.update_or_create(
-        perfil__user=user,
-        defaults={
-            "street_name": street_name,
-            "street_number": street_number,
-            "complement_address": complement_address,
-            "cellphone_number": cellphone_number,
-            "neighborhood_name": neighborhood_name,
-            "city_name": city_name,
-            "name_payer": name_payer,
-            "zip_code": zip_code 
-        }
-    )
     try:
         response = requests.put(f"{urlpix}/address/{id_address}", json=data)
         response.raise_for_status() 
