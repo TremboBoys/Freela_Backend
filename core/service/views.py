4febc7cdf9ba6a5a6978ccb1a37508d20a51abbc
kaubarros-24 <martinsbarroskaua@gmail.com>
2024-11-26 04:24:25 -0300

    CHORE: Serialized data in service

--- core/service/views.py
+++ core/service/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from core.perfil.models import Perfil
 from core.pay.models import Address
 from core.pay.use_case.pix import create_transaction
+from core.service.models import ContractService
 class ContractServiceAPIView(APIView):
     def post(self, request):
         objeto = request.data.get('objeto')
@@ -18,5 +19,23 @@ class ContractServiceAPIView(APIView):
             return Response({'message': err})
         
         return Response({"message": transaction}, status=status.HTTP_201_CREATED)
+    
+    def serialize_contract_service(obj):
+        contractor = obj.contractor.name if obj.contractor else None
+
+        return {
+            "id": obj.id,
+            "contractor": contractor,  
+            "type_of_service": obj.get_type_of_service_display(),
+            "is_paid": obj.is_paid,
+            "created_at": obj.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+
+    def get(self, request):
+        queryset = ContractService.objects.all()
+        serialized_data = [self.serialize_contract_service(c) for c in queryset]
+
+        return Response([serialized_data], safe=False)
+    
         
                 
