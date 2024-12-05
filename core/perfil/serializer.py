ef89ebd2456cdc0b1ddce3f13c909531e3bfd184
LuizBlume <luiz.fernando.251107@gmail.com>
2024-12-05 13:57:54 -0300

    FIX: Perfil serializer

--- core/perfil/serializer.py
+++ core/perfil/serializer.py
@@ -16,10 +16,24 @@ class UserNestedSerializer(ModelSerializer):
         
 class PerfilSerializer(ModelSerializer):
     user = UserNestedSerializer()
+    projects_in_execution = SerializerMethodField()
+    url_image = SerializerMethodField()
+    
     class Meta:
         model = Perfil
-        fields = ['balance', 'is_public', 'user', 'price_per_hour', 'nacionality', 'photo', 'payment_type', 'about_me', 'area', 'sub_area']
+        fields = ['balance', 'is_public', 'user', 'price_per_hour', 'nacionality', 'payment_type', 'about_me', 'area', 'sub_area', 'number_projects_in_execution', 'is_pro', 'access_token_mercado_pago', 'refresh_token_mercado_pago', 'expiration_date_access_token_mercado_pago', 'expiration_date_refresh_token_mercado_pago', 'collector_id_mercado_pago', 'avaliation', 'every_avaliations', 'image_perfil', 'projects_in_execution', 'url_image']
         depth = 1    
+    
+    def get_projects_in_execution(self, obj):
+        print(obj)
+        projects = AcceptProposal.objects.filter(proposal__perfil=obj, proposal__project__in_execution=True)
+        return len(projects)
+        
+    def get_url_image(self, obj):
+        if obj.image_perfil and obj.image_perfil.public_id:
+            url, _ = cloudinary_url(obj.image_perfil.public_id)
+            return url
+        return None
 class PerfilCurrentUserSerializer(ModelSerializer):
     projects_in_execution = SerializerMethodField()
     url_image = SerializerMethodField()
@@ -71,24 +85,24 @@ class ChoiceProjectSerializer(ModelSerializer):
     class Meta:
         model = ChoiceProject
         fields = "__all__" 
-class PerfilSerializer(ModelSerializer):
-    projects_in_execution = SerializerMethodField()
-    url_image = SerializerMethodField()
-    class Meta:
-        model = Perfil
-        fields = "__all__"
-        depth = 1
+# class PerfilSerializer(ModelSerializer):
+#     projects_in_execution = SerializerMethodField()
+#     url_image = SerializerMethodField()
+#     class Meta:
+#         model = Perfil
+#         fields = "__all__"
+#         depth = 1
         
-    def get_projects_in_execution(self, obj):
-        print(obj)
-        projects = AcceptProposal.objects.filter(proposal__perfil=obj, proposal__project__in_execution=True)
-        return len(projects)
+#     def get_projects_in_execution(self, obj):
+#         print(obj)
+#         projects = AcceptProposal.objects.filter(proposal__perfil=obj, proposal__project__in_execution=True)
+#         return len(projects)
         
-    def get_url_image(self, obj):
-        if obj.image_perfil and obj.image_perfil.public_id:
-            url, _ = cloudinary_url(obj.image_perfil.public_id)
-            return url
-        return None
+#     def get_url_image(self, obj):
+#         if obj.image_perfil and obj.image_perfil.public_id:
+#             url, _ = cloudinary_url(obj.image_perfil.public_id)
+#             return url
+#         return None
         
 class PerfilAvaliationSerializer(ModelSerializer):
     class Meta:
